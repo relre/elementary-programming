dbaac68843d1101b80e0936e548f9db2441f2451
relre <relre@outlook.com>
2024-03-27 22:48:38 +0300

    added ispowerof2

--- step1/ispowerof2/main.go
+++ step1/ispowerof2/main.go
@@ -2,11 +2,32 @@ package main
 
 import (
 	"os"
+	"strconv"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
-	if len(os.Args) > 1 {
-		return
+	if len(os.Args) == 2 {
+		nbr, _ := strconv.Atoi(os.Args[1])
+		for _, char := range isPowerOf(nbr) {
+			z01.PrintRune(char)
+		}
+		z01.PrintRune('\n')
+
 	}
 
+}
+
+func isPowerOf(n int) string {
+	if n == 0 || n == 1 {
+		return "false"
+	}
+	for n != 1 {
+		if n%2 != 0 {
+			return "false"
+		}
+		n /= 2
+	}
+	return "true"
 }
